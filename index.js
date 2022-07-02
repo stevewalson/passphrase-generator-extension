32fbc9fe7e89b27938ccf459d309de5eac66ca30
Steve Walson <75564431+stevewalson@users.noreply.github.com>
2022-07-02 18:15:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const passphrase_box = document.querySelector('.passphrase');
+const passphrase = document.querySelector('.passphrase'),
+generate_btn = document.querySelector('.generate');
+    copy_btn = document.querySelector('.copy');
 
-function passphrase(){
-    fetch("wordlist.txt")
-    .then(res => res.text())
-    .then(data =>{
-        let wordlist_to_array = data.split(" "),
-            random_number = Math.floor(Math.random() * (30 - 20) + 20).toFixed(0),
-            random_array_order = wordlist_to_array.sort(()=> 0.5 - Math.random()),
-            shuffle_random_result = random_array_order.slice(0,random_number),
-            join_words = shuffle_random_result.join(" ");
-            passphrase_box.innerText = join_words;
-            
+
+passphrase.innerText = localStorage.getItem('passphrase');
+
+    generate_btn.addEventListener("click",()=>{
+        fetch("wordlist.txt")
+        .then(res => res.text())
+        .then(data =>{
+            let wordlist_to_array = data.split(" "),
+                random_number = Math.floor(Math.random() * (30 - 20) + 20).toFixed(0),
+                random_array_order = wordlist_to_array.sort(()=> 0.5 - Math.random()),
+                shuffle_random_result = random_array_order.slice(0,random_number),
+                join_words = shuffle_random_result.join(" ");
+                passphrase.innerText = join_words;
+                localStorage.setItem('passphrase',join_words);
+                
+        })
     })
-}
-
-
-window.addEventListener('DOMcontentLoaded',passphrase());
+    copy_btn.addEventListener("click",()=>{
+        navigator.clipboard.writeText(passphrase.innerText);
+    })
